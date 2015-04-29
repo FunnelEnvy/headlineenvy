203bde4a0937f29a3b3fce3bfb02c959d05908ca
Jamie Poitra <jamie@gigaom.com>
2015-04-29 16:56:00 -0700

    Another minor tweak.

--- headline-envy.php
+++ headline-envy.php
@@ -6,7 +6,7 @@
  * Description: Leverages Optimizely to A/B test post headlines.
  * Author: FunnelEnvy
  * Author URI: http://www.funnelenvy.com/
- * Contributors: borkweb, zbtirrell
+ * Contributors: borkweb, zbtirrell, methnen
  * Tags: a/b testing
  * License: MIT
  * License URI: http://opensource.org/licenses/MIT
